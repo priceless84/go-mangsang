63e1d3e7430ecea3f16fbd2a19c24576a6c1c493
priceless84 <priceless84@naver.com>
2026-07-29 20:50:51 +0900

    Rename status check endpoint

--- server.js
+++ server.js
@@ -366,7 +366,7 @@ createServer(async (req, res) => {
       return;
     }
 
-    if (req.method === "GET" && url.pathname === "/monitor-status") {
+    if (req.method === "GET" && url.pathname === "/healthz") {
       json(res, 200, {
         ok: true,
         enabled: process.env.RENDER_MONITOR !== "false",
